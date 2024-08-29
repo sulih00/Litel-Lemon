2b3815e5e5652060bcc98bcc2913836a19ab5b92
sulih00 <sulih055@gmail.com>
2024-08-29 21:53:27 +0700

    e

--- src/App.test.js
+++ src/App.test.js
@@ -16,15 +16,11 @@ test('Renders the Header heading', () => {
     const headingElementNew = screen.getByText("Choose Date");
     expect(headingElementNew).toBeInTheDocument();
 })
-
 test('Initialize/Update Times', () => {
   render(<BrowserRouter><App /></BrowserRouter>);
   const reserveButton = screen.getByRole("button");
   fireEvent.click(reserveButton);
-
   const testTime = []
-  // userEvent.selectOptions(screen.getByLabelText("Choose Time"),screen.getByRole('option', { name: testTime}))
-  // expect(screen.getByRole('option', { name: testTime}).selected).toBe(true);
 
 
 })
